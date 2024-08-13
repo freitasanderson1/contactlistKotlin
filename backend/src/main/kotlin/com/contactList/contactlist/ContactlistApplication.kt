8dd8a60e59075ab6f4c9f465bafdbc457821172b
freitasanderson1 <andercsf1@gmail.com>
2024-08-12 21:35:12 -0300

    :zap: Add backend
    Add backend

---
+++ backend/src/main/kotlin/com/contactList/contactlist/ContactlistApplication.kt
@@ -0,0 +1,11 @@
+package com.contactList.contactlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContactlistApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContactlistApplication>(*args)
+}
